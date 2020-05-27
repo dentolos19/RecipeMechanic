1c7c193fad191165a7bcc27c272fdf07bb09ecfd
Dennise <63337204+dentolos19@users.noreply.github.com>
2020-05-27 13:29:36 +0800

    Finalized Requirement Modifier

--- Graphics/WnModify.xaml.cs
+++ Graphics/WnModify.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using SmRecipeModifier.Core.Models;
 
 namespace SmRecipeModifier.Graphics
@@ -46,17 +47,80 @@ namespace SmRecipeModifier.Graphics
 
         private void Add(object sender, RoutedEventArgs args)
         {
-            // todo
+            if (LvRequirements.Items.Count >= 3)
+            {
+                MessageBox.Show("You are only allowed max 3 requirements in a recipe.", "Scrap Mechanic doesn't allow that!", MessageBoxButton.OK, MessageBoxImage.Stop);
+                return;
+            }
+            MessageBox.Show("This function is not available yet.", "Sorry about that!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
 
         private void Remove(object sender, RoutedEventArgs args)
         {
-            // todo
+            if (LvRequirements.SelectedItem == null)
+                return;
+            if (LvRequirements.Items.Count == 1)
+            {
+                MessageBox.Show("You must have a minimum 1 requirement in a recipe.", "Scrap Mechanic doesn't allow that!", MessageBoxButton.OK, MessageBoxImage.Stop);
+                return;
+            }
+            MessageBox.Show("This function is not available yet.", "Sorry about that!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void RemoveRq(LvRequirementBinding binding)
+        {
+            lock (LvRequirements.Items)
+            {
+                foreach (var item in LvRequirements.Items.OfType<LvRequirementBinding>().ToArray())
+                    if (item.Id == binding.Id)
+                        LvRequirements.Items.Remove(item);
+            }
+        }
+
+        private bool CheckRqIdExists(string id)
+        {
+            var result = false;
+            foreach (var item in LvRequirements.Items.OfType<LvRequirementBinding>())
+                if (item.Id == id)
+                    result = true;
+            return result;
         }
 
         private void Modify(object sender, RoutedEventArgs args)
         {
-            // todo
+            if (LvRequirements.SelectedItem == null)
+                return;
+            var binding = LvRequirements.SelectedItem as LvRequirementBinding;
+            var dialog = new WnModifyRq(binding) { Owner = this };
+            if (dialog.ShowDialog() == false || dialog.Result == null)
+                return;
+            RemoveRq(binding);
+            if (CheckRqIdExists(binding?.Id))
+            {
+                MessageBox.Show("Duplicated requirement found! Please use another item.", "There can't be both!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                LvRequirements.Items.Add(binding);
+                return;
+            }
+            LvRequirements.Items.Add(dialog.Result);
+        }
+
+        private void Cancel(object sender, RoutedEventArgs args)
+        {
+            Close();
+        }
+
+        private void ItemSelectionUpdate(object sender, SelectionChangedEventArgs args)
+        {
+            if (LvRequirements.SelectedItem == null)
+            {
+                BnRemove.IsEnabled = false;
+                BnModify.IsEnabled = false;
+            }
+            else
+            {
+                BnRemove.IsEnabled = true;
+                BnModify.IsEnabled = true;
+            }
         }
 
     }
---
+++ Graphics/WnModifyRq.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using SmRecipeModifier.Core.Models;
+
+namespace SmRecipeModifier.Graphics
+{
+
+    public partial class WnModifyRq
+    {
+
+        public LvRequirementBinding Result { get; private set; }
+
+        public WnModifyRq(LvRequirementBinding binding)
+        {
+            InitializeComponent();
+            LbTitle.Content = binding.Name;
+            TbQuantity.Text = binding.Quantity.ToString();
+            foreach (var item in App.WindowMain.ItemDictionary.Items)
+                CbItem.Items.Add(new ComboBoxItem { Content = item.Name, Tag = item.Id });
+            CbItem.Text = binding.Name;
+        }
+
+        private void Cancel(object sender, RoutedEventArgs args)
+        {
+            Close();
+        }
+
+        private void Save(object sender, RoutedEventArgs args)
+        {
+            var item = CbItem.SelectedItem as ComboBoxItem;
+            Result = new LvRequirementBinding(int.Parse(TbQuantity.Text), item?.Content.ToString(), item?.Tag.ToString());
+            DialogResult = true;
+            Close();
+        }
+
+    }
+
+}
