5e25c703617d44aea495d389aea42c208e91b76f
Dennise Catolos <contact@dennise.me>
2020-07-05 10:52:20 +0800

    Fixed Developer Analytics

--- SmRecipeModifier/App.xaml.cs
+++ SmRecipeModifier/App.xaml.cs
@@ -23,19 +23,9 @@ namespace SmRecipeModifier
             Settings = Configuration.Load();
             var accent = Utilities.GetRandomAccent();
             Utilities.SetAppTheme(accent);
-            AppCenter.Start("fb87b876-50e7-41ac-a84b-a01f08c2a5f3", typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("deff7951-472f-4983-9d9e-cb073440e574", typeof(Analytics), typeof(Crashes));
             WindowMain = new WnMain();
             WindowMain.Show();
-        }
-
-        private void HandleExceptions(object sender, DispatcherUnhandledExceptionEventArgs args)
-        {
-            var answer = MessageBox.Show($"An error had occurred! {args.Exception.Message} Do you want to restart? Or else this program will just exit.", "Houston, we got a problem!", MessageBoxButton.YesNo, MessageBoxImage.Error);
-            if (answer != MessageBoxResult.Yes)
-                return;
-            args.Handled = true;
-            Process.Start(Assembly.GetExecutingAssembly().Location);
-            Current.Shutdown();
         }
 
     }
